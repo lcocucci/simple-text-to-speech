3073b790723c1ccd05564af9309e71aa6f3a09c1
lcocucci <leococucci@gmail.com>
2024-07-24 11:43:07 -0300

    Cargamos el modelo piper y implementamos docker con python 3.10 para solucionar conflictos con dependencias entre python 3.12 y piper-tts

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI, HTTPException
-# from api.tts import tts
-from services.tts_service import synthesize_with_google_tts 
+from fastapi.responses import StreamingResponse
 from models.item import SynthesisRequest
+from services.tts_service import synthesize_with_piper
+import base64
+import io
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'greetings': 'welcome to the FastAPI app'}
-
-
-@app.post("/tts")
-async def tts(request: SynthesisRequest):
-    # if request.method == "local":
-    #     audio = await synthesize_with_piper(request.text)
-    if request.method == "api":
-        audio = await synthesize_with_google_tts(request.text)
+@app.post("/synthesize")
+async def synthesize(request: SynthesisRequest):
+    if request.method == "local":
+        audio = await synthesize_with_piper(request.text)
     else:
         raise HTTPException(status_code=400, detail="Invalid method")
     
-    return {"audio": audio}
+    audio_buffer = io.BytesIO(base64.b64decode(audio))
+    return StreamingResponse(audio_buffer, media_type="audio/mp3")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Text-to-Speech API"}
--- services/tts_service.py
+++ services/tts_service.py
@@ -1,28 +1,17 @@
-from google.cloud import texttospeech
-# import piper
+from piper.voice import PiperVoice
+from pathlib import Path
 import base64
+import io
+import wave
 
-# model_path = "path/to/piper/model"
-# voice = piper.Voice(model_path)
+# Define the path to the model
+model_path = Path("/usr/src/app/piper-models/es_MX-claude-high.onnx")
+voice = PiperVoice.load(str(model_path))
 
-# async def synthesize_with_piper(text: str) -> str:
-#     audio = voice.synthesize(text)
-#     audio_base64 = base64.b64encode(audio).decode("utf-8")
-#     return audio_base64
-
-
-async def synthesize_with_google_tts(text: str) -> str:
-    client = texttospeech.TextToSpeechClient()
-    input_text = texttospeech.SynthesisInput(text=text)
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US",
-        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-    )
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
-    response = client.synthesize_speech(
-        input=input_text, voice=voice, audio_config=audio_config
-    )
-    audio_base64 = base64.b64encode(response.audio_content).decode("utf-8")
+async def synthesize_with_piper(text: str) -> str:
+    output_buffer = io.BytesIO()
+    with wave.open(output_buffer, "w") as wav_file:
+        voice.synthesize(text, wav_file)
+    output_buffer.seek(0)
+    audio_base64 = base64.b64encode(output_buffer.read()).decode("utf-8")
     return audio_base64
