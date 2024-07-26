549bfcb5664fe1c090e0becc771b2be8d0b56f0f
lcocucci <leococucci@gmail.com>
2024-07-26 11:19:47 -0300

    Actualizamos rama main para que implemente correctamente ambas funcionalidades

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 # from api.tts import tts
-from services.tts_service import synthesize_with_google_tts, synthesize_with_gtts 
+from services.tts_service import synthesize_with_piper, synthesize_with_gtts 
 from models.item import SynthesisRequest
 import base64
 import io
@@ -13,10 +13,12 @@ app = FastAPI()
 async def synthesize(request: SynthesisRequest):
     if request.method == "local":
         audio = await synthesize_with_piper(request.text)
+    elif request.method == "api":
+        audio = await synthesize_with_gtts(request.text)
     else:
         raise HTTPException(status_code=400, detail="Invalid method")
     
     audio_buffer = io.BytesIO(base64.b64decode(audio))
     return StreamingResponse(audio_buffer, media_type="audio/mp3")
     
-    return {"audio": audio}
+    # return {"audio": audio}
--- services/tts_service.py
+++ services/tts_service.py
@@ -1,17 +1,31 @@
 from piper.voice import PiperVoice
+from gtts import gTTS
 from pathlib import Path
 import base64
-import io
 import wave
+import io
+import datetime
 
-# Define the path to the model
+# Definimos el path en el que se encuentra el modelo de piper
 model_path = Path("/usr/src/app/piper-models/es_MX-claude-high.onnx")
 voice = PiperVoice.load(str(model_path))
 
+# Sísntesis de voz en local con piper
 async def synthesize_with_piper(text: str) -> str:
     output_buffer = io.BytesIO()
     with wave.open(output_buffer, "w") as wav_file:
         voice.synthesize(text, wav_file)
     output_buffer.seek(0)
     audio_base64 = base64.b64encode(output_buffer.read()).decode("utf-8")
+    return audio_base64
+
+# Síntesis de voz utilizando gTTS
+async def synthesize_with_gtts(text: str) -> str:
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    tts = gTTS(text, lang='es', tld='us')
+    tts.save(f'./samples/gtts-sample-{now}.mp3')
+    audio_buffer = io.BytesIO()
+    tts.write_to_fp(audio_buffer)
+    audio_buffer.seek(0)
+    audio_base64 = base64.b64encode(audio_buffer.read()).decode("utf-8")
     return audio_base64
